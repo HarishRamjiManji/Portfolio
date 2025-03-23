f51df97912c1f4b36dfe8718f060a3e29284a5de
HarishRamjiManji <ramjiharin@gmail.com>
2025-03-24 12:05:39 +1300

    actual final commit 0

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+
 import Header from "./Header";
 import Home from "../Pages/Home";
 import Projects from "../Pages/Projects";
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom'; // Import Router
 import './Styles/style1.css';
 import App from './components/App'; // Import your main App component
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />); // Render the full App
+root.render(
+  <Router basename="/">
+    <App />
+  </Router>
+);
