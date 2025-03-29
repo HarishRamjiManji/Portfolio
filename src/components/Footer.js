9158b29ce822ba31282320e23116e6f7ee1d895f
HarishRamjiManji <ramjiharin@gmail.com>
2025-03-30 11:13:10 +1300

    12

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => (
       
         <div className="footer_icons">
           <a href="mailto:harinbuildersltd@gmail.com" className="leftlink">
-          <img src="/rs/icons/gmail-svgrepo-com.png" alt="GMail" className="imgs3" />
+          <img src="rs/icons/gmail-svgrepo-com.png" alt="GMail" className="imgs3" />
           </a>
           <a href="https://www.linkedin.com/in/harish-ramji-manji-65b82373/" className="leftlink">
-          <img src="/rs/icons/linkedin-svgrepo-com.png" alt="LinkedIn" className="imgs3" />
+          <img src="rs/icons/linkedin-svgrepo-com.png" alt="LinkedIn" className="imgs3" />
           </a>
           <a href="https://github.com/HarishRamjiRaghwani" className="leftlink">
-          <img src="/rs/icons/git.jpg" alt="GitHub" className="imgs3" />
+          <img src="rs/icons/git.jpg" alt="GitHub" className="imgs3" />
           </a>
       </div>
       <p>
